dcc44d52bbcf7f7e0b460a631fe601405777f3c5
Gabryella Fachini <gabryellafachini@outlook.com>
2025-11-16 23:34:00 -0300

    Add user search functionality and user type definition
    Introduced a UserProps type for GitHub user data. Updated Home and Search components to support searching for a GitHub user by username and fetching user data from the GitHub API.

--- GitHubUsers/src/Routes/Home.tsx
+++ GitHubUsers/src/Routes/Home.tsx
@@ -1,7 +1,19 @@
 import Search from "../components/Search";
+import { useState } from "react";
+import { UserProps } from "../types/user";
 
 const Home = () => {
-    return <div>Home</div>
+    const [user, setUser] = useState<UserProps | null>(null);
+
+    const loadUser = async (username: string) => {
+        const res = await fetch(`https://api.github.com/users/${username}`);
+        const data = await res.json();
+        setUser(data);
+    }
+
+    return <div>
+        <Search loadUser={loadUser} />
+    </div>
 };
 
 export default Home;
--- GitHubUsers/src/components/Search.tsx
+++ GitHubUsers/src/components/Search.tsx
@@ -1,14 +1,25 @@
+type SearchProps = {
+    loadUser: (username: string) => Promise<void>;
+}
+
+import { useState } from "react";
 import { BsSearch } from "react-icons/bs";
 
-const Search = () => {
+const Search = ({ loadUser }: SearchProps) => {
+    const [username, setUsername] = useState("");
+
     return (
         <div>
             <h2>Find a user:</h2>
             <p>Search your best repositories</p>
             <div>
-                <input type="text" placeholder="Put the user name:" />
-                <button>
-                    <BsSearch></BsSearch>
+                <input type="text" 
+                placeholder="Put the user name:" 
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                />
+                <button onClick={() => loadUser(username)}>
+                    <BsSearch />
                 </button>
             </div>
         </div>
